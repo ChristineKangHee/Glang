9f75606f99aca0919e1d9c20abd3ea1456c1c3d9
ChristineKangHee <christinekangh522@gmail.com>
2025-01-15 17:21:09 +0900

    [Feat] writing_form_main.dart 추가(1/15)

--- lib/main.dart
+++ lib/main.dart
@@ -33,8 +33,8 @@ void main() async {
   }
 
   // local notification
-  await requestNotificationPermission();
-  await initializeNotifications();
+  // await requestNotificationPermission();
+  // await initializeNotifications();
 
   // Easy Localization 초기화
   await EasyLocalization.ensureInitialized();
