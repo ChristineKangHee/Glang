1514cf3e6deef31bb50991fe83475a1b8425543b
ParkMinjun0721 <83351998+ParkMinjun0721@users.noreply.github.com>
2025-01-16 15:46:17 +0900

    [Feat] 아이콘 추가, 로그인 화면 변경, 각 화면 SafeArea 설정 (25.01.16)

--- lib/view/login/login_main.dart
+++ lib/view/login/login_main.dart
@@ -1,12 +1,10 @@
-/// File: login_main.dart
-/// Purpose: Google 로그인을 위한 로그인 화면 구현 및 별명 설정 또는 홈 화면으로의 네비게이션 처리
-/// Author: 박민준
-/// Created: 2025-01-07
-/// Last Modified: 2025-01-07 by 박민준
-
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:readventure/theme/font.dart';
+import 'package:readventure/theme/theme.dart';
+import '../../viewmodel/custom_colors_provider.dart';
 import 'auth_controller.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends ConsumerWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -14,12 +12,19 @@ class LoginPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final authController = ref.watch(authControllerProvider.notifier);
+    final customColors = ref.watch(customColorsProvider); // CustomColors 가져오기
 
     return Scaffold(
+      backgroundColor: customColors.primary,
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: [
+            Expanded(
+              child: Center(
+                child: SvgPicture.asset("assets/icons/app_icon.svg"),
+              )
+            ),
             GestureDetector(
               onTap: () {
                 authController.signInWithGoogle(
@@ -31,8 +36,23 @@ class LoginPage extends ConsumerWidget {
                   },
                 );
               },
-              child: const Text('Google 로그인'),
+              child: GoogleLoginButton(customColors: customColors),
             ),
+            SizedBox(height: 16,),
+            GestureDetector(
+              onTap: () {
+                // authController.signInWithKakao(
+                //   onNicknameRequired: () {
+                //     Navigator.pushReplacementNamed(context, '/nickname');
+                //   },
+                //   onHome: () {
+                //     Navigator.pushReplacementNamed(context, '/');
+                //   },
+                // );
+              },
+              child: AppleLoginButton(customColors: customColors),
+            ),
+            SizedBox(height: 16,),
             GestureDetector(
               onTap: () {
                 authController.signInWithKakao(
@@ -44,8 +64,129 @@ class LoginPage extends ConsumerWidget {
                   },
                 );
               },
-              child: const Text('카카오 로그인'),
+              child: KakaoLoginButton(customColors: customColors),
             ),
+            SizedBox(height: 86,),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GoogleLoginButton extends StatelessWidget {
+  const GoogleLoginButton({
+    super.key,
+    required this.customColors,
+  });
+
+  final CustomColors customColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        width: 361,
+        height: 64,
+        decoration: BoxDecoration(
+          color: customColors.neutral100,
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(color: Color(0xFFCDCED3), width: 1.5),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 24,
+              height: 24,
+              // 구글 아이콘 뒤 그림자 현재 제대로 적용안됌
+              // decoration: const BoxDecoration(
+              //   boxShadow: [
+              //     BoxShadow(
+              //       color: Color.fromRGBO(0, 0, 0, 0.25),
+              //       blurRadius: 4,
+              //       offset: Offset(0, 4),
+              //       // spreadRadius: 0,
+              //     )
+              //   ],
+              // ),
+              child: Image.asset("assets/icons/google.png"),
+            ),
+            SizedBox(width: 16,),
+            Text("Google로 계속하기", style: body_medium_semi(context).copyWith(color: customColors.neutral30),)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AppleLoginButton extends StatelessWidget {
+  const AppleLoginButton({
+    super.key,
+    required this.customColors,
+  });
+
+  final CustomColors customColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        width: 361,
+        height: 64,
+        decoration: BoxDecoration(
+          color: customColors.neutral0,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 32,
+              height: 32,
+              child: Image.asset("assets/icons/apple_icon_white.png"),
+            ),
+            SizedBox(width: 16,),
+            Text("Apple로 계속하기", style: body_medium_semi(context).copyWith(color: customColors.neutral100),)
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class KakaoLoginButton extends StatelessWidget {
+  const KakaoLoginButton({
+    super.key,
+    required this.customColors,
+  });
+
+  final CustomColors customColors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: Container(
+        width: 361,
+        height: 64,
+        decoration: BoxDecoration(
+          color: Color(0xFFFAE100),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 32,
+              height: 32,
+              child: Image.asset("assets/icons/kakao_icon.png"),
+            ),
+            SizedBox(width: 16,),
+            Text("카카오로 계속하기", style: body_medium_semi(context).copyWith(color: customColors.neutral30),)
           ],
         ),
       ),
