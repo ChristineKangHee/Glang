cbc79efe3f9ddd0620e6573e87c0a7188f817cf0
ChristineKangHee <christinekangh522@gmail.com>
2025-01-30 11:35:47 +0900

    [Feat] notification_tab_bar inkwell 수정 (1/30)

--- lib/view/home/notification/components/notification_tab_bar.dart
+++ lib/view/home/notification/components/notification_tab_bar.dart
@@ -2,7 +2,7 @@
 /// Purpose: 알림 탭 화면에서 사용할 커스터마이즈된 TabBar 위젯을 제공하여 탭 전환 및 스타일을 관리
 /// Author: 박민준
 /// Created: 2025-01-02
-/// Last Modified: 2025-01-03 by 박민준
+/// Last Modified: 2025-01-30 by 강희
 
 import 'package:flutter/material.dart';
 import 'package:readventure/theme/font.dart';
@@ -40,6 +40,7 @@ class NotificationTabBar extends StatelessWidget implements PreferredSizeWidget 
                     context,
                     text: ['전체', '안읽음', '학습', '보상', '시스템'][index],
                     isSelected: isSelected,
+                    index: index, // 인덱스를 전달
                   );
                 }),
               ),
@@ -51,29 +52,37 @@ class NotificationTabBar extends StatelessWidget implements PreferredSizeWidget 
     );
   }
 
-  Widget _buildTab(BuildContext context, {required String text, required bool isSelected}) {
+  Widget _buildTab(BuildContext context, {required String text, required bool isSelected, required int index}) {
     final customColors = Theme.of(context).extension<CustomColors>()!;
 
-    return Container(
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      alignment: Alignment.center,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(12), // 둥근 모서리
-        border: Border.all(
-          color: isSelected
-              ? customColors.primary ?? Colors.blue // 선택된 탭의 테두리 색상
-              : customColors.neutral90 ?? Colors.grey, // 선택되지 않은 탭의 테두리 색상
+    return InkWell(
+      borderRadius: BorderRadius.circular(12), // 리플 효과에 둥근 모서리 적용
+      onTap: () {
+        tabController.animateTo(index); // 탭 클릭 시 해당 인덱스로 애니메이션 효과와 함께 전환
+      },
+      child: Container(
+        padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12), // 둥근 모서리
+          border: Border.all(
+            color: isSelected
+                ? customColors.primary ?? Colors.blue // 선택된 탭의 테두리 색상
+                : customColors.neutral90 ?? Colors.grey, // 선택되지 않은 탭의 테두리 색상
+          ),
         ),
-      ),
-      child: Text(
-        text,
-        textAlign: TextAlign.center,
-        style: body_xsmall_semi(context).copyWith(
-          color: isSelected ? customColors.white : customColors.neutral60, // 텍스트 색상을 동적으로 변경
+        child: Text(
+          text,
+          textAlign: TextAlign.center,
+          style: body_xsmall_semi(context).copyWith(
+            color: isSelected ? customColors.white : customColors.neutral60, // 텍스트 색상을 동적으로 변경
+          ),
         ),
       ),
     );
   }
+
+
 
   @override
   Size get preferredSize => const Size.fromHeight(58); // TabBar 높이 설정
